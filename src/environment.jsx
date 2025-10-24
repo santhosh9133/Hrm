b3e7d4764fc3dd4e6a6f3ef5a4980751d05bcc0e
Hrm Developer <developer@example.com>
2025-10-24 11:32:29 +0530

    Add core app files and utils/routes

---
+++ src/environment.jsx
@@ -0,0 +1,9 @@
+export const base_path = "/";
+export const image_path = '/reactjs/template/';
+
+// src/environment.jsx
+export const environment = {
+  API_URL: import.meta.env.VITE_API_URL,
+  APP_NAME: import.meta.env.VITE_APP_NAME,
+  MODE: import.meta.env.MODE,
+};
