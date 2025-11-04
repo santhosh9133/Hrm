f09190c3777b0e7755bcbe97c2a3a37c0092e992
richhmindxai <richhmindxai@gmail.com>
2025-11-04 13:50:29 +0530

    new

--- dist/assets/index-Cmq8TsEv.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-sNClTogd.js";import{C as p}from"./chips.esm-BmzoNdc4.js";const C=({value:o,onChange:n,placeholder:r="Type and enter",className:s})=>e.jsx(p,{className:s,value:o,onChange:t=>n(t.value),placeholder:r});export{C};
